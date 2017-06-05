bf82e9285e1ef379f53296df559626e6ff53545a
cjw85 <chris.wright@nanoporetech.com>
2017-06-05 09:57:38 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, re
+import sys, os, re, subprocess
 import sphinx_rtd_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
